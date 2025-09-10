d2fd8f2767dc5a8d7cb1cc8a7a83f12636a95258
Gabriel Marinho <101935392+gabrielmarinho1@users.noreply.github.com>
2025-09-10 21:38:20 +0000

    app.py-update

--- app.py
+++ app.py
@@ -1,10 +1,3 @@
-# estrutura do banco de dados:
-
-# produtos = {
-#     1: {"nome": "Produto A", "preco": 10.0, "quantidade": 100, "categoria": "Eletrônicos"},
-#     2: {"nome": "Produto B", "preco": 20.0, "quantidade": 50, "categoria": "Roupas"},
-#     3: {"nome": "Produto C", "preco": 15.0, "quantidade": 75, "categoria": "Alimentos"}
-# }
 from flask import Flask, request, jsonify
 import json
 
